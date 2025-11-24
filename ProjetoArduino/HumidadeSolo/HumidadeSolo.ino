a1f971b373c11ff886a44fc7f18120d6e385457f
Jeffreypir <jefferson1995mat@gmail.com>
2025-11-24 10:28:50 -0300

    Atualizando dicas para apresentar

--- ProjetoArduino/HumidadeSolo/HumidadeSolo.ino
+++ ProjetoArduino/HumidadeSolo/HumidadeSolo.ino
@@ -1,4 +1,6 @@
 // Código para testar o sensor de umidade do solo HD-38
+
+#include <Arduino.h>
 #define SENSOR_PINO A0  // Pino analógico conectado ao sensor
 
 void setup() {
@@ -10,13 +12,13 @@ void setup() {
 void loop() {
   int leitura = analogRead(SENSOR_PINO); // Lê o valor do sensor
   int porcentagem = map(leitura, 0, 1023, 100, 0); // Converte para porcentagem
-  
+
   Serial.print("Leitura bruta: ");
   Serial.print(leitura);
   Serial.print(" | Umidade: ");
   Serial.print(porcentagem);
   Serial.println("%");
-  
+
   // Classifica a umidade
   if (porcentagem >= 70) {
     Serial.println("Solo muito úmido");
@@ -27,7 +29,7 @@ void loop() {
   } else {
     Serial.println("Solo muito seco");
   }
-  
+
   Serial.println("---------------------------------------");
   delay(2000); // Espera 2 segundos entre as leituras
 }
